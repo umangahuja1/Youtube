fe4f87d39834690bb9f7f52a61e7e515f4b558d2
Umang Ahuja <umangahuja1@gmail.com>
2017-12-26 16:19:39 +0530

    Create saavn2.py

---
+++ Web_Scraping/Saavn/saavn2.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+
+res = requests.get('https://www.saavn.com/s/featured/hindi/Weekly_Top_Songs')
+
+soup = BeautifulSoup(res.text, 'lxml')
+
+data = soup.find('ol', {'class': 'content-list'})
+all_songs = data.find_all('div', {'class': 'details'})
+
+for count, s in enumerate(all_songs, 1):
+    song = s.find('p', {'class': 'song-name'})
+    print(count, song.text)
