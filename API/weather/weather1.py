6309e88c605e86019cd298deef500b1a1d3b2bd8
Umang Ahuja <umangahuja1@gmail.com>
2018-04-08 18:52:42 +0530

    Create weather1.py

---
+++ API/weather/weather1.py
@@ -0,0 +1,24 @@
+import requests
+from pprint import pprint
+
+city = input('Enter your city : ')
+
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=ac7c75b9937a495021393024d0a90c44&units=metric'.format(city)
+
+res = requests.get(url)
+
+data = res.json()
+
+temp = data['main']['temp']
+wind_speed = data['wind']['speed']
+
+latitude = data['coord']['lat']
+longitude = data['coord']['lon']
+
+description = data['weather'][0]['description']
+
+print('Temperature : {} degree celcius'.format(temp))
+print('Wind Speed : {} m/s'.format(wind_speed))
+print('Latitude : {}'.format(latitude))
+print('Longitude : {}'.format(longitude))
+print('Description : {}'.format(description))
