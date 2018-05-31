8b156480a7e332c9d02661fc56f161800bf03a82
Umang Ahuja <umangahuja1@gmail.com>
2018-05-31 20:03:24 +0530

    Create weather2.py

---
+++ API/weather/weather2.py
@@ -0,0 +1,28 @@
+import requests
+
+res = requests.get('https://ipinfo.io/')
+data = res.json()
+
+location = data['loc'].split(',')
+latitude = location[0]
+longitude = location[1]
+
+url = 'http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid=ac7c75b9937a495021393024d0a90c44&units=metric'.format(latitude, longitude)
+
+res = requests.get(url)
+
+data = res.json()
+
+temp = data['main']['temp']
+wind_speed = data['wind']['speed']
+
+latitude = data['coord']['lat']
+longitude = data['coord']['lon']
+
+description = data['weather'][0]['description']
+
+print('Temperature : {} degree celcius'.format(temp))
+print('Wind Speed : {} m/s'.format(wind_speed))
+print('Latitude : {}'.format(latitude))
+print('Longitude : {}'.format(longitude))
+print('Description : {}'.format(description))
