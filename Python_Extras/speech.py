9fd2f990cf4bc0ee86167a61b1824d29a2fe4bc9
Umang Ahuja <umangahuja1@gmail.com>
2018-06-13 03:11:52 +0530

    Create speech.py

---
+++ Python_Extras/speech.py
@@ -0,0 +1,11 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Speak Anything :")
+    audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        print("You said : {}".format(text))
+    except:
+        print("Sorry could not recognize what you said")
